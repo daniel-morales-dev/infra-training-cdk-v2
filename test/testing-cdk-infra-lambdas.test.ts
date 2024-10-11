03c0c5f9ec941f8b10ef72c14eaffbbb42599050
Daniel Morales <daniel.morales@alegra.com>
2024-10-11 14:10:05 -0500

    Initial commit

---
+++ test/testing-cdk-infra-lambdas.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as TestingCdkInfraLambdas from '../lib/testing-cdk-infra-lambdas-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/testing-cdk-infra-lambdas-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new TestingCdkInfraLambdas.TestingCdkInfraLambdasStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
